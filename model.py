3fdcbdb93db07c004a0788f04d6998b5c8094327
Sayan Mondal <92223435+Silverflame1@users.noreply.github.com>
2023-11-11 14:20:54 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,504 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import math
+from modules import *
+
+train_loss = []
+val_loss = []
+i=0
+
+class DiffusionModel(pl.LightningModule):
+    def __init__(self, in_size, t_range, img_depth):
+        super().__init__()
+        self.beta_small = 1e-4
+        self.beta_large = 0.02
+        self.t_range = t_range
+        self.in_size = in_size
+
+        bilinear = True
+        self.inc = DoubleConv(img_depth, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        factor = 2 if bilinear else 1
+        self.down3 = Down(256, 512 // factor)
+        self.up1 = Up(512, 256 // factor, bilinear)
+        self.up2 = Up(256, 128 // factor, bilinear)
+        self.up3 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, img_depth)
+        self.sa1 = SAWrapper(256, 8)
+        self.sa2 = SAWrapper(256, 4)
+        self.sa3 = SAWrapper(128, 8)
+
+    def pos_encoding(self, t, channels, embed_size):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc.view(-1, channels, 1, 1).repeat(1, 1, embed_size, embed_size)
+
+    def forward(self, x, t):
+        """
+        Model is U-Net with added positional encodings and self-attention layers.
+        """
+        x1 = self.inc(x)
+        x2 = self.down1(x1) + self.pos_encoding(t, 128, 16)
+        x3 = self.down2(x2) + self.pos_encoding(t, 256, 8)
+        x3 = self.sa1(x3)
+        x4 = self.down3(x3) + self.pos_encoding(t, 256, 4)
+        x4 = self.sa2(x4)
+        x = self.up1(x4, x3) + self.pos_encoding(t, 128, 8)
+        x = self.sa3(x)
+        x = self.up2(x, x2) + self.pos_encoding(t, 64, 16)
+        x = self.up3(x, x1) + self.pos_encoding(t, 64, 32)
+        output = self.outc(x)
+        return output
+
+    def beta(self, t):
+        return self.beta_small + (t / self.t_range) * (
+            self.beta_large - self.beta_small
+        )
+
+    def alpha(self, t):
+        return 1 - self.beta(t)
+
+    def alpha_bar(self, t):
+        return math.prod([self.alpha(j) for j in range(t)])
+
+    def get_loss(self, batch, batch_idx):
+        """
+        Corresponds to Algorithm 1 from (Ho et al., 2020).
+        """
+        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        noise_imgs = []
+        epsilons = torch.randn(batch.shape, device=self.device)
+        for i in range(len(ts)):
+            a_hat = self.alpha_bar(ts[i])
+            noise_imgs.append(
+                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+            )
+        noise_imgs = torch.stack(noise_imgs, dim=0)
+        e_hat = self.forward(noise_imgs, ts.unsqueeze(-1).type(torch.float))
+        loss = nn.functional.mse_loss(
+            e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
+        )
+        return loss
+
+    def denoise_sample(self, x, t):
+        """
+        Corresponds to the inner loop of Algorithm 2 from (Ho et al., 2020).
+        """
+        with torch.no_grad():
+            if t > 1:
+                z = torch.randn(x.shape)
+            else:
+                z = 0
+            e_hat = self.forward(x, t.view(1, 1).repeat(x.shape[0], 1))
+            pre_scale = 1 / math.sqrt(self.alpha(t))
+            e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
+            post_sigma = math.sqrt(self.beta(t)) * z
+            x = pre_scale * (x - e_scale * e_hat) + post_sigma
+            return x
+
+    def training_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("train/loss", loss,on_step=False, on_epoch = True, prog_bar=True, logger=True, enable_graph = True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("val/loss", loss)
+        return
+    
+    def on_epoch(self, trainer, pl_module):
+        train_acc = pl_module.train_acc.compute()
+        val_acc = pl_module.val_acc.compute()
+        train_loss[i] = trainer.callback_metrics['train/loss']
+        val_loss[i] = trainer.callback_metrics['val/loss']
+        i = i+ 1
+        #print(f'Epoch {trainer.current_epoch}: train_loss={train_loss:.4f}, val_loss={val_loss:.4f}, train_acc={train_acc:.4f}, val_acc={val_acc:.4f}')
+        print('train_loss =', trainer.callback_metrics['train/loss'])
+        print('val_loss=', trainer.callback_metrics['val/loss'])
+        print('train_acc= ',train_acc)
+        
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        return optimizer
+
+
+
+
+
+# UNET 2++
+
+class DiffusionModel_2(pl.LightningModule):
+    def __init__(self, in_size, t_range, img_depth):
+        super().__init__()
+        self.beta_small = 1e-4
+        self.beta_large = 0.02
+        self.t_range = t_range
+        self.in_size = in_size
+
+        bilinear = True
+        self.inc = DoubleConv(img_depth, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        factor = 2 if bilinear else 1
+        self.down3 = Down(256, 512 // factor)
+
+        self.P3 = DoubleConv(256,256)
+        self.P2 = DoubleConv(128,128)
+        self.P1 = DoubleConv(64,64)
+
+        self.up1 = Up(512, 256 // factor, bilinear)
+        self.up2 = Up(256, 128 // factor, bilinear)
+        self.up3 = Up(128, 64, bilinear)
+
+        self.outc = OutConv(64, img_depth)
+
+        self.sa1 = SAWrapper(256, 8)
+        self.sa2 = SAWrapper(256, 4)
+        self.sa3 = SAWrapper(128, 8)
+
+    def pos_encoding(self, t, channels, embed_size):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc.view(-1, channels, 1, 1).repeat(1, 1, embed_size, embed_size)
+
+    def forward(self, x, t):
+        x1 = self.inc(x)
+        print(x1.shape)
+        x2 = self.down1(x1) + self.pos_encoding(t, 128, 16)
+        print(x2.shape)
+        x3 = self.down2(x2) + self.pos_encoding(t, 256, 8)
+        print(x3.shape)
+        x3 = self.sa1(x3)
+        x4 = self.down3(x3) + self.pos_encoding(t, 256, 4)
+        print(x4.shape)
+        x4 = self.sa2(x4)
+
+        x = self.up1(x4, self.P3(x3)) + self.pos_encoding(t, 128, 8)
+        print(x.shape)
+        x = self.sa3(x)
+        x = self.up2(x, self.P2(x2)) + self.pos_encoding(t, 64, 16)
+        print(x.shape)
+        x = self.up3(x, self.P1(x1)) + self.pos_encoding(t, 64, 32)
+        print(x.shape)
+
+        output = self.outc(x)
+        return output
+
+    def beta(self, t):
+        return self.beta_small + (t / self.t_range) * (
+            self.beta_large - self.beta_small
+        )
+
+    def alpha(self, t):
+        return 1 - self.beta(t)
+
+    def alpha_bar(self, t):
+        return math.prod([self.alpha(j) for j in range(t)])
+
+    def get_loss(self, batch, batch_idx):
+      
+        #Corresponds to Algorithm 1 from (Ho et al., 2020).
+
+        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        noise_imgs = []
+        epsilons = torch.randn(batch.shape, device=self.device)
+        for i in range(len(ts)):
+            a_hat = self.alpha_bar(ts[i])
+            noise_imgs.append(
+                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+            )
+        noise_imgs = torch.stack(noise_imgs, dim=0)
+        e_hat = self.forward(noise_imgs, ts.unsqueeze(-1).type(torch.float))
+        loss = nn.functional.mse_loss(
+            e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
+        )
+        return loss
+
+    def denoise_sample(self, x, t):
+
+        #Corresponds to the inner loop of Algorithm 2 from (Ho et al., 2020).
+   
+        with torch.no_grad():
+            if t > 1:
+                z = torch.randn(x.shape)
+            else:
+                z = 0
+            e_hat = self.forward(x, t.view(1, 1).repeat(x.shape[0], 1))
+            pre_scale = 1 / math.sqrt(self.alpha(t))
+            e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
+            post_sigma = math.sqrt(self.beta(t)) * z
+            x = pre_scale * (x - e_scale * e_hat) + post_sigma
+            return x
+
+    def training_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("train/loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("val/loss", loss)
+        return
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        return optimizer
+    
+
+
+#UNET 3
+
+class DiffusionModel_3(pl.LightningModule):
+    def __init__(self, in_size, t_range, img_depth):
+        super().__init__()
+        self.beta_small = 1e-4
+        self.beta_large = 0.02
+        self.t_range = t_range
+        self.in_size = in_size
+
+        bilinear = True
+        self.inc = DoubleConv(img_depth, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        factor = 2 if bilinear else 1
+        self.down3 = Down(256, 512 // factor)
+
+        self.P4 = DoubleConv(512,512)
+        self.P3 = DoubleConv(256,256)
+        self.P2 = DoubleConv(128,128)
+        self.P1 = DoubleConv(64,64)
+        
+        #self.P6 = DoubleConv(512,512)
+        self.P5 = DoubleConv(256,256)
+        
+        self.up1 = Up(512, 256 // factor, bilinear)
+        self.up2 = Up(256, 128 // factor, bilinear)
+        self.up3 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, img_depth)
+        self.sa1 = SAWrapper(256, 8)
+        self.sa2 = SAWrapper(256, 4)
+        self.sa3 = SAWrapper(128, 8)
+
+    def pos_encoding(self, t, channels, embed_size):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc.view(-1, channels, 1, 1).repeat(1, 1, embed_size, embed_size)
+
+
+    def forward(self, x, t):
+        x1 = self.inc(x)
+        print(x1.shape)
+        x2 = self.down1(x1) + self.pos_encoding(t, 128, 16)
+        print(x2.shape)
+        x3 = self.down2(x2) + self.pos_encoding(t, 256, 8)
+        print(x3.shape)
+        x3 = self.sa1(x3)
+        x4 = self.down3(x3) + self.pos_encoding(t, 256, 4)
+        print(x4.shape)
+        x4 = self.sa2(x4)
+
+        x = self.up1(x4, self.P5(self.P3(x3))) + self.pos_encoding(t, 128, 8)
+        print(x.shape)
+        x = self.sa3(x)
+        x = self.up2(x, self.P2(x2)) + self.pos_encoding(t, 64, 16)
+        print(x.shape)
+        x = self.up3(x, self.P1(x1)) + self.pos_encoding(t, 64, 32)
+        print(x.shape)
+
+        output = self.outc(x)
+        return output
+
+
+    def beta(self, t):
+        return self.beta_small + (t / self.t_range) * (
+            self.beta_large - self.beta_small
+        )
+
+    def alpha(self, t):
+        return 1 - self.beta(t)
+
+    def alpha_bar(self, t):
+        return math.prod([self.alpha(j) for j in range(t)])
+
+    def get_loss(self, batch, batch_idx):
+        """
+        Corresponds to Algorithm 1 from (Ho et al., 2020).
+        """
+        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        noise_imgs = []
+        epsilons = torch.randn(batch.shape, device=self.device)
+        for i in range(len(ts)):
+            a_hat = self.alpha_bar(ts[i])
+            noise_imgs.append(
+                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+            )
+        noise_imgs = torch.stack(noise_imgs, dim=0)
+        e_hat = self.forward(noise_imgs, ts.unsqueeze(-1).type(torch.float))
+        loss = nn.functional.mse_loss(
+            e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
+        )
+        return loss
+
+    def denoise_sample(self, x, t):
+        """
+        Corresponds to the inner loop of Algorithm 2 from (Ho et al., 2020).
+        """
+        with torch.no_grad():
+            if t > 1:
+                z = torch.randn(x.shape)
+            else:
+                z = 0
+            e_hat = self.forward(x, t.view(1, 1).repeat(x.shape[0], 1))
+            pre_scale = 1 / math.sqrt(self.alpha(t))
+            e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
+            post_sigma = math.sqrt(self.beta(t)) * z
+            x = pre_scale * (x - e_scale * e_hat) + post_sigma
+            return x
+
+    def training_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("train/loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("val/loss", loss)
+        return
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        return optimizer
+
+
+# more depth
+class DiffusionModel_d1(pl.LightningModule):
+    def __init__(self, in_size, t_range, img_depth):
+        super().__init__()
+        self.beta_small = 1e-4
+        self.beta_large = 0.02
+        self.t_range = t_range
+        self.in_size = in_size
+
+        filters = [64, 128, 256, 512, 1024]
+
+        bilinear = True
+        self.inc = DoubleConv(img_depth, 64)
+        self.down1 = Down(filters[0], filters[1])
+        self.down2 = Down(filters[1], filters[2])
+        self.down3 = Down(filters[2], filters[3])
+        factor = 2 if bilinear else 1
+        self.down4 = Down(filters[3], filters[4]// factor)
+        self.up0 = Up(filters[4], filters[3]// factor, bilinear)
+        self.up1 = Up(filters[3], filters[2] // factor, bilinear)
+        self.up2 = Up(filters[2], filters[1] // factor, bilinear)
+        self.up3 = Up(filters[1], filters[0], bilinear)
+        self.outc = OutConv(filters[0], img_depth)
+        self.sa1 = SAWrapper(256, 8)
+        self.sa2 = SAWrapper(256, 4)
+        self.sa3 = SAWrapper(128, 8)
+
+    def pos_encoding(self, t, channels, embed_size):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc.view(-1, channels, 1, 1).repeat(1, 1, embed_size, embed_size)
+
+    def forward(self, x, t):
+        """
+        Model is U-Net with added positional encodings and self-attention layers.
+        """
+        x1 = self.inc(x)
+        x2 = self.down1(x1) + self.pos_encoding(t, 128, 16)
+        x3 = self.down2(x2) + self.pos_encoding(t, 256, 8)
+        x3 = self.sa1(x3)
+        x4 = self.down3(x3) + self.pos_encoding(t, 256, 4)
+        x4 = self.sa2(x4)
+        x5 = self.down4(x4) + self.pos_encoding(t, 256, 4)
+        x = self.up0(x5, x4) + self.pos_encoding(t, 128, 8)
+        x = self.sa3(x)
+        x = self.up1(x, x3) + self.pos_encoding(t, 128, 8)
+        x = self.up2(x, x2) + self.pos_encoding(t, 64, 16)
+        x = self.up3(x, x1) + self.pos_encoding(t, 64, 32)
+        output = self.outc(x)
+        return output
+
+    def beta(self, t):
+        return self.beta_small + (t / self.t_range) * (
+            self.beta_large - self.beta_small
+        )
+
+    def alpha(self, t):
+        return 1 - self.beta(t)
+
+    def alpha_bar(self, t):
+        return math.prod([self.alpha(j) for j in range(t)])
+
+    def get_loss(self, batch, batch_idx):
+        """
+        Corresponds to Algorithm 1 from (Ho et al., 2020).
+        """
+        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        noise_imgs = []
+        epsilons = torch.randn(batch.shape, device=self.device)
+        for i in range(len(ts)):
+            a_hat = self.alpha_bar(ts[i])
+            noise_imgs.append(
+                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+            )
+        noise_imgs = torch.stack(noise_imgs, dim=0)
+        e_hat = self.forward(noise_imgs, ts.unsqueeze(-1).type(torch.float))
+        loss = nn.functional.mse_loss(
+            e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
+        )
+        return loss
+
+    def denoise_sample(self, x, t):
+        """
+        Corresponds to the inner loop of Algorithm 2 from (Ho et al., 2020).
+        """
+        with torch.no_grad():
+            if t > 1:
+                z = torch.randn(x.shape)
+            else:
+                z = 0
+            e_hat = self.forward(x, t.view(1, 1).repeat(x.shape[0], 1))
+            pre_scale = 1 / math.sqrt(self.alpha(t))
+            e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
+            post_sigma = math.sqrt(self.beta(t)) * z
+            x = pre_scale * (x - e_scale * e_hat) + post_sigma
+            return x
+
+    def training_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("train/loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.get_loss(batch, batch_idx)
+        self.log("val/loss", loss)
+        return
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        return optimizer
